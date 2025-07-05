0c65ec7a3bb0e1928201d6b73b58c58f3dd4f346
anggiapws <anggiaputriwulansuci@gmail.com>
2025-07-05 14:00:15 +0700

    Update app.py

--- lamaran_app 2/app.py
+++ lamaran_app 2/app.py
@@ -166,4 +166,5 @@ def logout():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # ambil port dari Glitch, default 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
